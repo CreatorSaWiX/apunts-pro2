8f9bf0e17ec2ae508ef81d1588b7672e958b1bda
Àlex Xavier Touza Luque <alex.touza.l@gmail.com>
2026-02-10 22:59:37 +0100

    fix: Declare CopyButton component outside CodeBlock

--- src/components/ui/CodeBlock.tsx
+++ src/components/ui/CodeBlock.tsx
@@ -1,10 +1,10 @@
-import { Copy, Check } from 'lucide-react';
-import React, { useState, useEffect } from 'react';
+import {Copy, Check} from 'lucide-react';
+import React, {useState, useEffect} from 'react';
 import Prism from 'prismjs';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-c';
 import 'prismjs/components/prism-cpp';
-import { languageNames, type LanguageColors } from '../../utils/languages.ts';
+import {languageNames, type LanguageColors} from '../../utils/languages.ts';
 import clsx from 'clsx'
 
 interface CodeBlockProps {
@@ -22,8 +22,21 @@ const languageColorsClass = {
 } as const satisfies Record<LanguageColors, string>;
 
 
+const CopyButton = ({title, handleCopy, copied, className}: {
+    title?: string,
+    handleCopy: () => void,
+    copied: boolean,
+    className?: string
+}) =>
+    <button
+        onClick={handleCopy}
+        className={clsx(`p-2 rounded-lg text-slate-300 hover:text-white hover:bg-white/10 hover:opacity-100 transition-all opacity-80 z-10 ${title ? 'top-1.5' : 'top-3'}`, className)}
+        title="Copiar codi"
+    >
+        {copied ? <Check size={16} className="text-emerald-400"/> : <Copy size={16}/>}
+    </button>;
 
-const CodeBlock: React.FC<CodeBlockProps> = ({ code, title, language = 'cpp' }) => {
+const CodeBlock: React.FC<CodeBlockProps> = ({code, title, language = 'cpp'}) => {
     const [copied, setCopied] = useState(false);
     const codeRef = React.useRef<HTMLElement>(null);
 
@@ -39,27 +52,25 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ code, title, language = 'cpp' }) 
         setTimeout(() => setCopied(false), 2000);
     };
 
-    const CopyButton = ({ className }: { className?: string }) => <button
-        onClick={handleCopy}
-        className={clsx(`p-2 rounded-lg text-slate-300 hover:text-white hover:bg-white/10 hover:opacity-100 transition-all opacity-80 z-10 ${title ? 'top-1.5' : 'top-3'}`, className)}
-        title="Copiar codi"
-    >
-        {copied ? <Check size={16} className="text-emerald-400" /> : <Copy size={16} />}
-    </button>;
 
     return (
-        <div className="flex flex-col rounded-2xl relative group bg-slate-900/70 border border-white/10 h-full overflow-hidden">
+        <div
+            className="flex flex-col rounded-2xl relative group bg-slate-900/70 border border-white/10 h-full overflow-hidden">
 
             {title ? (
-                <div className="flex gap-2 rounded-t-2xl items-center justify-between bg-linear-to-r from-blue-800/20 to-slate-800/5 px-4 py-1 border-b border-white/10">
-                    <span className="text-sm font-medium text-slate-300 font-mono">{title}</span>
-                    <span className={`font-mono opacity-80 hover:opacity-100 transition px-1 ml-auto  rounded-md border  ${languageColorsClass[languageNames[language].color]}`}>{languageNames[language].name}</span>
+                    <div
+                        className="flex gap-2 rounded-t-2xl items-center justify-between bg-linear-to-r from-blue-800/20 to-slate-800/5 px-4 py-1 border-b border-white/10">
+                        <span className="text-sm font-medium text-slate-300 font-mono">{title}</span>
+                        <span
+                            className={`font-mono opacity-80 hover:opacity-100 transition px-1 ml-auto  rounded-md border  ${languageColorsClass[languageNames[language].color]}`}>{languageNames[language].name}</span>
 
-                    <CopyButton />
-                </div>
-            ) :
+                        <CopyButton title={title}
+                                    copied={copied} handleCopy={handleCopy}/>
+                    </div>
+                ) :
 
                 <CopyButton
+                    copied={copied} handleCopy={handleCopy}
                     className={`absolute right-3 p-2 bg-white/5`}
                 />
             }
